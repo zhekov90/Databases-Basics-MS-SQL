3f85dfa0591bfa7af3cb219f40f4f03eee7b80e5
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-09 22:23:21 +0200

    task added

--- 09. Additional Exercises/09. Additional Exercises.sql
+++ 09. Additional Exercises/09. Additional Exercises.sql
@@ -14,3 +14,17 @@ JOIN Games AS g ON g.Id = ug.GameId
 JOIN GameTypes AS gt ON gt.Id = g.GameTypeId
 JOIN Characters AS c ON c.Id = ug.CharacterId
 ORDER BY ug.[Level] DESC, u.Username, g.[Name]
+
+
+--Problem 3.	Users in Games with Their Items
+
+SELECT u.Username, g.[Name] AS Game, COUNT(i.Id) AS [Items Count], SUM(i.Price) AS [Items Price] FROM  UsersGames AS ug
+JOIN Users AS u ON u.Id = ug.UserId
+JOIN Games AS g ON g.Id = ug.GameId
+JOIN UserGameItems AS ugi ON ugi.UserGameId = ug.Id
+JOIN Items AS i ON i.Id = ugi.ItemId
+GROUP BY u.Username, g.[Name]
+HAVING COUNT(i.Id) >= 10
+ORDER BY COUNT(i.Id) DESC, [Items Price] DESC, u.Username
+
+
