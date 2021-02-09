4827af77b48fe77f643a0c5d7889c99a6ee8e0ab
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-09 22:07:56 +0200

    task added

--- 09. Additional Exercises/09. Additional Exercises.sql
+++ 09. Additional Exercises/09. Additional Exercises.sql
@@ -6,3 +6,11 @@ GROUP BY SUBSTRING([Email], CHARINDEX('@', [Email]) + 1, LEN([Email]))
 ORDER BY [Number Of Users] DESC, [Email Provider]
 
 
+--Problem 2.	All User in Games
+
+SELECT g.[Name] AS Game, gt.[Name] AS [Game Type], u.Username, ug.[Level], ug.Cash, c.[Name] FROM  UsersGames AS ug
+JOIN Users AS u ON u.Id = ug.UserId
+JOIN Games AS g ON g.Id = ug.GameId
+JOIN GameTypes AS gt ON gt.Id = g.GameTypeId
+JOIN Characters AS c ON c.Id = ug.CharacterId
+ORDER BY ug.[Level] DESC, u.Username, g.[Name]
