a2dab2514f9826efe968422afceea17e23df0deb
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-09 21:21:20 +0200

    added task

--- 08. Triggers and Transactions/08. Triggers and Transactions.sql
+++ 08. Triggers and Transactions/08. Triggers and Transactions.sql
@@ -188,3 +188,27 @@ INSERT INTO EmployeesProjects
 (@EmloyeeId, @ProjectID)
  
  COMMIT
+
+
+ -- 9.	Delete Employees
+
+DROP TABLE Deleted_Employees
+
+CREATE TABLE Deleted_Employees
+(
+ EmployeeId INT PRIMARY KEY IDENTITY,
+ FirstName VARCHAR(50), 
+ LastName VARCHAR(50), 
+ MiddleName VARCHAR(50), 
+ JobTitle VARCHAR(50), 
+ DeparmentId INT, 
+ Salary MONEY
+)
+
+GO
+
+CREATE TRIGGER tr_DeleteEmployee ON Employees AFTER DELETE
+AS
+INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+SELECT d.FirstName, d.LastName, d.MiddleName, d.JobTitle, d.DepartmentID, d.Salary 
+FROM deleted AS d
