327099c9be454c82cf96963c677b4be4ed69cfcb
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-09 21:04:55 +0200

    task added

--- 08. Triggers and Transactions/08. Triggers and Transactions.sql
+++ 08. Triggers and Transactions/08. Triggers and Transactions.sql
@@ -56,3 +56,29 @@ AS
 		SET Balance += @moneyAmount
 		WHERE Id = @accountId
 	COMMIT
+
+GO
+
+
+-- 4.	Withdraw Money
+
+CREATE PROCEDURE usp_WithdrawMoney(@accountId INT, @moneyAmount MONEY)
+AS
+BEGIN TRANSACTION
+
+	UPDATE Accounts
+	SET Balance -= @moneyAmount
+	WHERE Id = @accountId
+	
+	DECLARE @currentBalance money = (SELECT Balance FROM Accounts WHERE Id = @accountId)
+
+	IF(@currentBalance < 0)
+		BEGIN
+			ROLLBACK
+			RAISERROR('Insufficient funds!', 16, 1)
+			RETURN
+		END
+	ELSE
+		BEGIN
+			COMMIT
+		END
