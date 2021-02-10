f560da99ed601316e980a5b46883f191e0182670
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-10 15:07:58 +0200

    task added

--- Exams/Exam Preparation 2 - School/Exam Preparation 2 - School.sql
+++ Exams/Exam Preparation 2 - School/Exam Preparation 2 - School.sql
@@ -71,3 +71,20 @@ CONSTRAINT FK_StudentsSubjects_Subjects FOREIGN KEY (SubjectId) REFERENCES Subje
 )
 
 
+--2. Insert
+
+INSERT INTO Teachers(FirstName, LastName,	[Address],	Phone,	SubjectId)
+VALUES
+('Ruthanne', 'Bamb', '84948 Mesta Junction', '3105500146', 6),
+('Gerrard', 'Lowin', '370 Talisman Plaza', '3324874824', 2),
+('Merrile', 'Lambdin', '81 Dahle Plaza	', '4373065154', 5),
+('Bert', 'Ivie', '2 Gateway Circle', '4409584510', 4)
+
+INSERT INTO Subjects([Name], Lessons)
+VALUES
+('Geometry', 12),
+('Health', 10),
+('Drama', 7),
+('Sports', 9)
+
+
