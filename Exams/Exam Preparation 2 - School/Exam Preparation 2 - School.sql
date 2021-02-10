e9277be63c6f5d5ae0017152ec58c8281625ad8c
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-10 15:52:28 +0200

    wrong last task fixed

--- Exams/Exam Preparation 2 - School/Exam Preparation 2 - School.sql
+++ Exams/Exam Preparation 2 - School/Exam Preparation 2 - School.sql
@@ -130,7 +130,10 @@ ORDER BY [Full Name]
 
 --8. Top Students
 
-SELECT s.FirstName, s.LastName, FORMAT(AVG(se.Grade), 'N2') AS Grade  FROM Students AS s
+SELECT TOP(10) s.FirstName, s.LastName, FORMAT(AVG(se.Grade), 'N2') AS Grade  FROM Students AS s
 JOIN StudentsExams AS se ON se.StudentId = s.Id
 GROUP BY s.FirstName, s.LastName
 ORDER BY Grade DESC, s.FirstName, s.LastName
+
+
+--
