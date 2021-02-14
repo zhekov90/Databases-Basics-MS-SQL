8b505986122c4909d7123dc9c7418db1ce37ed25
Zdravko Zhekov <hheelloo@abv.bg>
2021-02-14 14:37:42 +0200

    Issue Assignment task added

--- Exams/Exam - 13 February 2021 Bitbucket/Exam - 13 February 2021 Bitbucket.sql
+++ Exams/Exam - 13 February 2021 Bitbucket/Exam - 13 February 2021 Bitbucket.sql
@@ -104,3 +104,10 @@ WHERE Size > 1000 AND [Name] LIKE '%html%'
 ORDER BY Size DESC, Id, [Name]
 
 
+--7.	Issue Assignment
+
+SELECT i.Id, CONCAT(u.Username, ' : ', i.Title) AS IssueAssignee FROM Issues AS i
+JOIN Users AS u ON u.Id = i.AssigneeId
+ORDER BY i.Id DESC, i.AssigneeId
+
+
